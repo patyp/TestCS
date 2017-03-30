a666a4598424f16822f90200ba1140c0bb162feb
Paty <Paty@PATY-VAIO>
2017-03-30 10:23:49 +0200

    added new line

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
+        }
+    }
+}
